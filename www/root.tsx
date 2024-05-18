f61dc847a9177cd59f28499766ebcf27c8f58108
dim <web.best.master@gmail.com>
2024-05-18 15:29:22 +0200

    WIP

--- www/root.tsx
+++ www/root.tsx
@@ -1,4 +1,8 @@
 import {sayHi} from "./const";
+import * as rootStyle from "./css/root.scss";
 
 // eslint-disable-next-line jest/require-hook
 sayHi();
+
+// eslint-disable-next-line jest/require-hook
+console.log({...rootStyle}, rootStyle.some_class_name, rootStyle.another_class_name);
